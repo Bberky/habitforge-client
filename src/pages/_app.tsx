b1590bc7cd079bab3057f22f9cc4b13292c55290
Adam Berkeš <berkeada@fit.cvut.cz>
2023-12-15 20:41:57 +0100

    Use Inter font throughout the whole app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/index.scss";
+import { inter } from "@/styles/fonts";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>HabitForge</title>
+      </Head>
+
+      <main className={`${inter.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
